aae305a3b2b0607df5ca91e07b43ce618a23d5fa
Muriloo3124 <mrilovb@gmail.com>
2023-05-08 17:05:30 -0300

    a

--- TCC_Pizzaria/Controller1/PedidoController.cs
+++ TCC_Pizzaria/Controller1/PedidoController.cs
@@ -1,9 +1,9 @@
-﻿using model1;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using model1;
 
 namespace Controller1
 {
--- TCC_Pizzaria/model1/Pedido.cs
+++ TCC_Pizzaria/model1/Pedido.cs
@@ -6,25 +6,26 @@ using System.Threading.Tasks;
 
 namespace model1
 {
-    public class Pedido
-    {
-
-        public int Id { get; set; }
-        public string Produto {get; set;}
-        public string Nome_cliente { get; set;}
-        public string Telefone { get; set;}
-        public string Endereco { get; set;}
-        public string Descricao { get; set;}
-
-        public Pedido()
+        public class Pedido
         {
-            this.Id = 0;
-            this.Produto = string.Empty;
-            this.Nome_cliente = string.Empty;
-            this.Telefone = string.Empty;
-            this.Endereco = string.Empty;
-            this.Descricao = string.Empty;
 
+            public int Id { get; set; }
+            public string Produto { get; set; }
+            public string Nome_cliente { get; set; }
+            public string Telefone { get; set; }
+            public string Endereco { get; set; }
+            public string Descricao { get; set; }
+
+            public Pedido()
+            {
+                this.Id = 0;
+                this.Produto = string.Empty;
+                this.Nome_cliente = string.Empty;
+                this.Telefone = string.Empty;
+                this.Endereco = string.Empty;
+                this.Descricao = string.Empty;
+
+            }
         }
-    }
+  
 }
